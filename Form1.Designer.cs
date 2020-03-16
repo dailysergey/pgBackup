084f5439d788e7e26883ee6071b8abbd3a4db201
dailysergey <ds@ru>
2020-03-16 09:52:41 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WikiForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.path_textBox = new System.Windows.Forms.TextBox();
+            this.openBut = new System.Windows.Forms.Button();
+            this.backupBut = new System.Windows.Forms.Button();
+            this.restoreBut = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.backup_dumpBut = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // path_textBox
+            // 
+            this.path_textBox.Location = new System.Drawing.Point(54, 29);
+            this.path_textBox.Name = "path_textBox";
+            this.path_textBox.Size = new System.Drawing.Size(353, 20);
+            this.path_textBox.TabIndex = 0;
+            // 
+            // openBut
+            // 
+            this.openBut.Location = new System.Drawing.Point(422, 25);
+            this.openBut.Name = "openBut";
+            this.openBut.Size = new System.Drawing.Size(90, 27);
+            this.openBut.TabIndex = 1;
+            this.openBut.Text = "открыть";
+            this.openBut.UseVisualStyleBackColor = true;
+            this.openBut.Click += new System.EventHandler(this.OpenBut_Click);
+            // 
+            // backupBut
+            // 
+            this.backupBut.Location = new System.Drawing.Point(54, 78);
+            this.backupBut.Name = "backupBut";
+            this.backupBut.Size = new System.Drawing.Size(137, 27);
+            this.backupBut.TabIndex = 2;
+            this.backupBut.Text = "backup sql";
+            this.backupBut.UseVisualStyleBackColor = true;
+            this.backupBut.Click += new System.EventHandler(this.BackupBut_Click);
+            // 
+            // restoreBut
+            // 
+            this.restoreBut.Location = new System.Drawing.Point(375, 78);
+            this.restoreBut.Name = "restoreBut";
+            this.restoreBut.Size = new System.Drawing.Size(137, 27);
+            this.restoreBut.TabIndex = 3;
+            this.restoreBut.Text = "restore";
+            this.restoreBut.UseVisualStyleBackColor = true;
+            this.restoreBut.Click += new System.EventHandler(this.RestoreBut_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // backup_dumpBut
+            // 
+            this.backup_dumpBut.Location = new System.Drawing.Point(219, 78);
+            this.backup_dumpBut.Name = "backup_dumpBut";
+            this.backup_dumpBut.Size = new System.Drawing.Size(137, 27);
+            this.backup_dumpBut.TabIndex = 4;
+            this.backup_dumpBut.Text = "backup dump";
+            this.backup_dumpBut.UseVisualStyleBackColor = true;
+            this.backup_dumpBut.Click += new System.EventHandler(this.Backup_dumpBut_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 126);
+            this.Controls.Add(this.backup_dumpBut);
+            this.Controls.Add(this.restoreBut);
+            this.Controls.Add(this.backupBut);
+            this.Controls.Add(this.openBut);
+            this.Controls.Add(this.path_textBox);
+            this.Name = "Form1";
+            this.Text = "WikiBackup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox path_textBox;
+        private System.Windows.Forms.Button openBut;
+        private System.Windows.Forms.Button backupBut;
+        private System.Windows.Forms.Button restoreBut;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button backup_dumpBut;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,257 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WikiForm
+{
+    public partial class Form1 : Form
+    {
+        private string pathToPG_RESTORE = ConfigurationManager.AppSettings["PathToPG_RESTOREv11"];
+        private string pathToPG_DUMP = ConfigurationManager.AppSettings["PathToPG_DUMPv11"];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Select saving folder with backups
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenBut_Click(object sender, EventArgs e)
+        {
+            string newDirectoryPath = string.Empty;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            else
+            {
+                newDirectoryPath = folderBrowserDialog1.SelectedPath;
+            }
+            path_textBox.Text = newDirectoryPath;
+        }
+
+        private void BackupBut_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(path_textBox.Text))
+            {
+                MessageBox.Show("Выберите папку для сохранения!");
+                return;
+            }
+            
+            var outputFileWithPg_DUMP = path_textBox.Text;
+            var host = ConfigurationManager.AppSettings["host"];
+            var port  = ConfigurationManager.AppSettings["port"];
+            var user = ConfigurationManager.AppSettings["user"];
+            var dbname = ConfigurationManager.AppSettings["dbname"];
+            var password = ConfigurationManager.AppSettings["password"];
+            Pg_dump_sql(
+                pathToPG_DUMP,
+                outputFileWithPg_DUMP,
+                host,
+                port,
+                dbname,
+                user,
+                password);
+        }
+
+        /// <summary>
+        /// <para>Using pg_dump makes dump in sql with inserts commands and save in selected dir.</para>
+        /// File log.txt is created to watch errors.If it is empty - everything is ok.
+        /// </summary>
+        /// <param name="pathToPG_DUMP">Полный путь до pg_dump.exe</param>
+        /// <param name="outFile"></param>
+        /// <param name="host"></param>
+        /// <param name="port"></param>
+        /// <param name="database"></param>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
+        private void Pg_dump_sql(
+            string pathToPG_DUMP,
+            string outFile,
+            string host,
+            string port,
+            string database,
+            string user,
+            string password
+            )
+        {
+            try
+            {
+                outFile = Path.Combine(outFile, $"{DateTime.Now.ToString("yyyy.MM.dd")}_wiki.sql");
+                ProcessStartInfo startinfo = new ProcessStartInfo
+                {
+                    FileName = pathToPG_DUMP,
+                    Arguments = $" --inserts --dbname=postgresql://{user}:{password}@{host}:{port}/{database} -f {outFile} ",
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = Process.Start(startinfo))
+                {
+                    using (StreamReader reader = process.StandardError)
+                    {
+                        StreamWriter sw = new StreamWriter(Path.Combine(path_textBox.Text, "log.txt"));
+                        sw.WriteLine(reader.ReadToEnd());
+                        sw.Close();
+                    }
+                }
+                MessageBox.Show("Success");
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show
+                    (e.Message);
+            }
+        }
+
+        /// <summary>
+        /// <para>Using pg_dump makes dump in dump ext with inserts commands and save in selected dir.</para>
+        /// File log.txt is created to watch errors.If it is empty - everything is ok.
+        /// </summary>
+        /// <param name="pathToPG_DUMP">Полный путь до pg_dump.exe</param>
+        /// <param name="outFile"></param>
+        /// <param name="host"></param>
+        /// <param name="port"></param>
+        /// <param name="database"></param>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
+        private void Pg_dump(
+            string pathToPG_DUMP,
+            string outFile,
+            string host,
+            string port,
+            string database,
+            string user,
+            string password
+            )
+        {
+            try
+            {
+                outFile = Path.Combine(outFile, $"{DateTime.Now.ToString("yyyy.MM.dd")}_wiki.dump");
+                ProcessStartInfo startinfo = new ProcessStartInfo
+                {
+                    FileName = pathToPG_DUMP,
+                    Arguments = $" --format=custom --dbname=postgresql://{user}:{password}@{host}:{port}/{database} -f {outFile} ",
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = Process.Start(startinfo))
+                {
+                    using (StreamReader reader = process.StandardError)
+                    {
+                        StreamWriter sw = new StreamWriter(Path.Combine(path_textBox.Text, "log.txt"));
+                        sw.WriteLine(reader.ReadToEnd());
+                        sw.Close();
+                    }
+                }
+                MessageBox.Show("Success");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show
+                    (e.Message);
+            }
+        }
+        
+        private void RestoreBut_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Dump files (*.dump) | *.dump|All files(*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // get selected file
+            string dumpFile = openFileDialog1.FileName;
+            if (string.IsNullOrEmpty(dumpFile))
+                return;
+            var host = ConfigurationManager.AppSettings["host"];
+            var port = ConfigurationManager.AppSettings["port"];
+            var user = ConfigurationManager.AppSettings["user"];
+            var dbname = ConfigurationManager.AppSettings["dbname"];
+            var password = ConfigurationManager.AppSettings["password"];
+            RestoreDump(pathToPG_RESTORE,dumpFile,host,port, dbname, user,password);
+        }
+
+        /// <summary>
+        /// Restore db from dump file with key -c (clears old db) 
+        /// </summary>
+        /// <param name="PathToPG_RESTORE"></param>
+        /// <param name="dumpFile"></param>
+        /// <param name="host"></param>
+        /// <param name="port"></param>
+        /// <param name="dbname"></param>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
+        private void RestoreDump(
+            string PathToPG_RESTORE,
+            string dumpFile,
+            string host,
+            string port,
+            string dbname,
+            string user,
+            string password)
+        {
+            try
+            {
+                ProcessStartInfo startinfo = new ProcessStartInfo
+                {
+                    FileName = PathToPG_RESTORE,
+                    Arguments = $" -c --dbname=postgresql://{user}:{password}@{host}:{port}/{dbname} {dumpFile}",
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = Process.Start(startinfo))
+                {
+                    using (StreamReader reader = process.StandardError)
+                    {
+                        StreamWriter sw = new StreamWriter(Path.Combine(path_textBox.Text, "log.txt"));
+                        sw.WriteLine(reader.ReadToEnd());
+                        sw.Close();
+                    }
+                }
+                MessageBox.Show("Success");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show
+                    (e.Message);
+            }
+        }
+
+        private void Backup_dumpBut_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(path_textBox.Text))
+            {
+                MessageBox.Show("Выберите папку для сохранения!");
+                return;
+            }
+            var outputFileWithPg_DUMP = path_textBox.Text;
+            var host = ConfigurationManager.AppSettings["host"];
+            var port = ConfigurationManager.AppSettings["port"];
+            var user = ConfigurationManager.AppSettings["user"];
+            var dbname = ConfigurationManager.AppSettings["dbname"];
+            var password = ConfigurationManager.AppSettings["password"];
+            Pg_dump(
+                pathToPG_DUMP,
+                outputFileWithPg_DUMP,
+                host,
+                port,
+                dbname,
+                user,
+                password);
+        }
+    }
+}
